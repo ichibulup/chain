e60e3872b4ea3e8610e8e07d7c85a55245ca654f
goraria <gorariagroup@gmail.com>
2026-03-07 12:02:19 +0700

    fix

--- apps/api/api/index.js
+++ apps/api/api/index.js
@@ -1,7 +1,7 @@
 require('express');
 
 // const appModule = require('../.express/app/index.js');
-const appModule = __importDefault(require("../.express/app/index.js"));
-// const appModule = require('/var/task/apps/api/api/index.js');
+// const appModule = __importDefault(require("../.express/app/index.js"));
+const appModule = require('/var/task/apps/api/api/index.js');
 
 module.exports = appModule.default || appModule;
